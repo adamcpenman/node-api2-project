88c62a63489061e588e283b0a73c903df5b6f18f
Adam <adamcpenman@gmail.com>
2019-12-07 12:08:53 -0600

    can update and create

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.get("/", (req, res) => {
         message: "Node API 2"
     })
 })
-
+//find all users
 server.get("/api/posts", (req, res) => {
     db.find()
     .then(posts => {
@@ -26,7 +26,7 @@ server.get("/api/posts", (req, res) => {
             .json({ error: "Can not find messages" })
     })
 })
-
+//find users by id
 server.get("/api/posts/:id" , (req, res) => {
     db.findById(req.params.id)
     .then(posts => {
@@ -39,6 +39,43 @@ server.get("/api/posts/:id" , (req, res) => {
             .status(404)
             .json({ error: "Can not find that particular ID"})
     })
+})
+
+//create a new post 
+server.post("/api/posts", (req, res) => {
+    if (!req.body.title || !req.body.contents) {
+        return res 
+            .status(400).json({ error: "Need a title and contents"})
+    }
+    const newPost = {
+        title: req.body.title,
+        contents: req.body.contents
+    }
+    db.insert(newPost)
+        res
+            .status(201)
+            .json(newPost)
+})
+
+//update 
+server.put("/api/posts/:id", (req, res) => {
+    if (!req.body.title) {
+        return res.status(400).json({ message: "Missing title" })
+    }
+    db.update(req.params.id, req.body) 
+        .then(change => {
+            if(change) {
+                res.status(200).json(change)
+            } else {
+                res.status(404).json({ message: "Nothing can be found"})
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({
+                message: "Error updating"
+            })
+        })
 })
 
 server.listen(4000, () => {
